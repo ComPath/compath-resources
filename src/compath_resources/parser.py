fcbfa7dc599d6a1225dd66ac9b854a36dadd95b1
ddomingof <danieldomingofernandez@hotmail.com>
2018-11-20 12:05:30 +0100

    Make reactome hiearchy optional

--- src/compath_resources/parser.py
+++ src/compath_resources/parser.py
@@ -14,12 +14,22 @@ __all__ = [
 ]
 
 
-def get_df() -> pd.DataFrame:
-    """Get all data in a frame."""
-    return pd.concat([
+def get_df(reactome=None) -> pd.DataFrame:
+    """Get all dataframes.
+
+    :param reactome: includes reactome hiearchy
+    :return: dataframe with ComPath dataset
+    """
+    compath_dataset = pd.concat([
         pd.read_csv(KEGG_WIKIPATHWAYS_URL),
         pd.read_csv(KEGG_REACTOME_URL),
         pd.read_csv(WIKIPATHWAYS_REACTOME_URL),
         pd.read_csv(SPECIAL_MAPPINGS_URL, usecols=list(range(7))),
-        pd.read_csv(REACTOME_HIERARCHICAL_MAPPINGS_URL),
     ])
+
+    if reactome:
+        pd.concat([
+            compath_dataset,
+            pd.read_csv(REACTOME_HIERARCHICAL_MAPPINGS_URL),
+        ])
+    return compath_dataset
