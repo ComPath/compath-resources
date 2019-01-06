b307a63cabe64ff4e543da065c8a0e57b1780c8b
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-01-06 17:11:24 +0100

    Make dataframe getters reusable functions

--- src/compath_resources/parser.py
+++ src/compath_resources/parser.py
@@ -11,23 +11,48 @@ from compath_resources.constants import (
 
 __all__ = [
     'get_df',
+    'get_kegg_reactome_df',
+    'get_kegg_wikipathways_df',
+    'get_wikipathways_reactome_df',
+    'get_reactome_hierarchy_df',
+    'get_special_mappings_df',
 ]
 
 
 def get_df(reactome: bool = False) -> pd.DataFrame:
     """Get all dataframes.
 
-    :param reactome: includes reactome hiearchy
+    :param reactome: include Reactome hiearchy?
     :return: dataframe with ComPath dataset
     """
     dfs = [
-        pd.read_csv(KEGG_WIKIPATHWAYS_URL),
-        pd.read_csv(KEGG_REACTOME_URL),
-        pd.read_csv(WIKIPATHWAYS_REACTOME_URL),
-        pd.read_csv(SPECIAL_MAPPINGS_URL, usecols=list(range(7))),
+        get_kegg_wikipathways_df(),
+        get_kegg_reactome_df(),
+        get_wikipathways_reactome_df(),
+        get_special_mappings_df(),
     ]
 
     if reactome:
-        dfs.append(pd.read_csv(REACTOME_HIERARCHICAL_MAPPINGS_URL))
+        dfs.append(get_reactome_hierarchy_df())
 
     return pd.concat(dfs)
+
+
+def get_kegg_wikipathways_df() -> pd.DataFrame:
+    return pd.read_csv(KEGG_WIKIPATHWAYS_URL)
+
+
+def get_kegg_reactome_df() -> pd.DataFrame:
+    return pd.read_csv(KEGG_REACTOME_URL)
+
+
+def get_wikipathways_reactome_df() -> pd.DataFrame:
+    return pd.read_csv(WIKIPATHWAYS_REACTOME_URL)
+
+
+def get_special_mappings_df() -> pd.DataFrame:
+    return pd.read_csv(SPECIAL_MAPPINGS_URL, usecols=list(range(7)))
+
+
+def get_reactome_hierarchy_df() -> pd.DataFrame:
+    return pd.read_csv(REACTOME_HIERARCHICAL_MAPPINGS_URL)
