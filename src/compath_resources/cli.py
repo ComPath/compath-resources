87987e1611f805953c0aa51376afae3e7e2f7aef
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-01-31 15:47:24 +0100

    Update export

--- src/compath_resources/cli.py
+++ src/compath_resources/cli.py
@@ -7,12 +7,11 @@ from typing import Optional, TextIO
 
 import click
 from compath_resources.manager import get_bel
-from pybel import to_pickle
 
 
 @click.command()
 @click.option('-o', '--output', help='file path to write')
-@click.option('-f', '--fmt', type=click.Choice(['bel', 'rdf']))
+@click.option('-f', '--fmt', type=click.Choice(['bel', 'rdf', 'nodelink']), default='bel')
 def main(output: Optional[TextIO], fmt: str):
     """Export ComPath resources."""
     graph = get_bel()
@@ -21,10 +20,10 @@ def main(output: Optional[TextIO], fmt: str):
     if output is None:
         sys.exit(0)
 
-    if fmt is None or fmt == 'bel':
-        to_pickle(graph, output)
-    elif fmt == 'rdf':
+    if fmt == 'rdf':
         click.echo('rdf export not implemented yet')
+    else:
+        graph.serialize(fmt, output)
 
 
 if __name__ == '__main__':
