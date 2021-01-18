8f50daa1259d06f1d6b74bc156957a3b86d86f71
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-18 21:18:09 +0100

    Add checks for identifier matching regex

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -3,6 +3,7 @@
 """Test data integrity."""
 
 import os
+import re
 import unittest
 
 import bioregistry
@@ -18,8 +19,13 @@ class TestIntegrity(unittest.TestCase):
     def setUp(self) -> None:
         """Set up integrity tests."""
         self.registry = bioregistry.read_bioregistry()
-        self.registry['decopath'] = ...  # TODO decopath needs a real resource and an entry in the bioregistry
+        self.registry['decopath'] = {}  # TODO decopath needs a real resource and an entry in the bioregistry
         self.x = {}
+        self.miriam_patterns = {
+            k: re.compile(entry['miriam']['pattern'])
+            for k, entry in self.registry.items()
+            if 'miriam' in entry
+        }
         for name in os.listdir(MAPPINGS_DIRECTORY):
             if name.endswith('.tsv'):
                 print(name)
@@ -35,15 +41,27 @@ class TestIntegrity(unittest.TestCase):
             with self.subTest(name=name):
                 for i, (source_prefix, source_id, target_prefix, target_id) in enumerate(df[rows].values):
                     self.assertIn(source_prefix, self.registry.keys())
+                    self.assertNotEqual(source_prefix, 'kegg')
                     self.assertFalse(
                         bioregistry.is_deprecated(source_prefix),
                         msg=f'[{name}, row {i}] deprecated source prefix: {source_prefix}',
                     )
+                    if source_regex := self.miriam_patterns.get(source_prefix):
+                        self.assertRegex(
+                            source_id, source_regex,
+                            msg=f'[{name}, row {i}] source prefix: {source_prefix}',
+                        )
                     self.assertIn(target_prefix, self.registry.keys())
+                    self.assertNotEqual(target_prefix, 'kegg')
                     self.assertFalse(
                         bioregistry.is_deprecated(target_prefix),
                         msg=f'[{name}, row {i}] deprecated target prefix: {target_prefix}',
                     )
+                    if target_regex := self.miriam_patterns.get(target_prefix):
+                        self.assertRegex(
+                            target_id, target_regex,
+                            msg=f'[{name}, row {i}] target prefix: {target_prefix}',
+                        )
 
     def test_decopath_export(self):
         """Test the exported data is the same as the excel."""
