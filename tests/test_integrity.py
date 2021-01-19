10ad495431af0d05a96310c2ba3399545d2e28d2
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-19 11:40:14 +0100

    Automatically import all mappings and fix formats
    Closes #22 Closess #15

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -6,14 +6,36 @@ import unittest
 
 import pandas as pd
 
-from compath_resources.convert_decopath import DECOPATH_TSV_PATH, get_decopath_df
+from compath_resources.import_mappings import (
+    _import_decopath_df, _import_kegg_reactome_df, _import_kegg_wikipathways_df, _import_pathbank_kegg_df,
+    _import_pathbank_reactome_df, _import_pathbank_wikipathways_df, _import_reactome_hierarchy_df,
+    _import_special_mappings_df, _import_wikipathways_reactome_df,
+)
+from compath_resources.resources import (
+    get_decopath_df, get_kegg_reactome_df, get_kegg_wikipathways_df, get_pathbank_kegg_df, get_pathbank_reactome_df,
+    get_pathbank_wikipathways_df, get_reactome_hierarchy_df, get_special_mappings_df, get_wikipathways_reactome_df,
+)
 
 
 class TestIntegrity(unittest.TestCase):
     """Test case for checking data integrity."""
 
-    def test_decopath_export(self):
+    def test_import(self):
         """Test the exported data is the same as the excel."""
-        xlsx_df = get_decopath_df()
-        tsv_df = pd.read_csv(DECOPATH_TSV_PATH, sep='\t')
-        self.assertTrue((xlsx_df.values == tsv_df.values).all())
+        for importer, getter in [
+            (_import_decopath_df, get_decopath_df),
+            (_import_kegg_reactome_df, get_kegg_reactome_df),
+            (_import_pathbank_reactome_df, get_pathbank_reactome_df),
+            (_import_pathbank_kegg_df, get_pathbank_kegg_df),
+            (_import_wikipathways_reactome_df, get_wikipathways_reactome_df),
+            (_import_reactome_hierarchy_df, get_reactome_hierarchy_df),
+            (_import_special_mappings_df, get_special_mappings_df),
+            (_import_pathbank_wikipathways_df, get_pathbank_wikipathways_df),
+            (_import_kegg_wikipathways_df, get_kegg_wikipathways_df),
+        ]:
+            with self.subTest(name=importer.__name__):
+                xlsx_df = importer()
+                self.assertIsInstance(xlsx_df, pd.DataFrame)
+                tsv_df = getter()
+                self.assertIsInstance(tsv_df, pd.DataFrame)
+                self.assertTrue((xlsx_df.values == tsv_df.values).all())
