908a3ddc8e7c6ddffc5bd997241db9259fd8766a
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-27 12:16:32 +0200

    Remove manager import
    something funky going on with sqlalchemy

--- src/compath_resources/__init__.py
+++ src/compath_resources/__init__.py
@@ -3,5 +3,4 @@
 """Utilities for ComPath's resources."""
 
 from .exporters import get_bel, get_rdf  # noqa: F401
-from .manager import Manager  # noqa: F401
 from .resources import get_df  # noqa: F401
